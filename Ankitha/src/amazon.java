8d820a350c0ff2edba9a5094e5de02c9be265a74
ankithap97 <83691827+ankithap97@users.noreply.github.com>
2021-07-09 10:59:42 +0530

    Initial commit to master

--- Ankitha/src/amazon.java
+++ Ankitha/src/amazon.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
@@ -6,17 +8,27 @@ import org.openqa.selenium.opera.OperaDriver;
 public class amazon {
 
 	public static void main(String[] args) {
-		/*System.setProperty("webdriver.opera.driver", "D:\\operadriver_win64\\operadriver_win64\\operadriver.exe");
-        WebDriver driver =new OperaDriver();
-        driver.get("https://www.amazon.in/");
-        String s=driver.getTitle();
-        System.out.println(s);
-        driver.quit();*/
-		System.setProperty("webdriver.ie.driver", "D:\\IEDriverServer.exe");
-        WebDriver driver =new InternetExplorerDriver();
-        driver.get("https://www.amazon.in/");
-        driver.quit();
-        
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		 WebDriver driver =new ChromeDriver();
+		 // TC1 Open url
+	        driver.get("https://www.amazon.in/");
+	     // TC2 maximize the windows
+	        driver.manage().window().maximize();
+	     // TC3 Verify The search functionality 
+	        WebElement a=driver.findElement(By.id("twotabsearchtextbox"));
+	        a.click();
+	        a.sendKeys("Asus mobile");
+	        driver.findElement(By.id("nav-search-submit-button")).click();
+	      //TC4 Navigate back to home page
+	        driver.navigate().back();
+	      //TC5 To print the title of homepage
+	      String s=  driver.getTitle();
+	      System.out.println(s);
+	      //TC6 close the browser
+	       driver.quit();
+	      
+        	 
+         }
 	}
 
-}
+
