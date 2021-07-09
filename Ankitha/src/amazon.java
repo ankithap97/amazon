4e1af353b36c0c99defe460e8d42e9dbfaa44c43
ankithap97 <83691827+ankithap97@users.noreply.github.com>
2021-07-09 09:36:03 +0530

    initial commit to master

---
+++ Ankitha/src/amazon.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+public class amazon {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.opera.driver", "D:\\operadriver_win64\\operadriver_win64\\operadriver.exe");
+        WebDriver driver =new OperaDriver();
+        driver.get("https://www.amazon.in/");
+        String s=driver.getTitle();
+        System.out.println(s);
+        driver.quit();
+	}
+
+}
